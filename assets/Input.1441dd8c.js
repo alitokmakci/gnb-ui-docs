07949d4da11b0684fb1fb94133b27bad62f5dc6f
Ali Tokmakcı <alitokmakci@Ali-MacBook-Pro.modem.local>
2022-08-19 14:10:26 +0300

    deploy

---
+++ assets/Input.1441dd8c.js
@@ -0,0 +1,120 @@
+import{_ as c}from"./Layout.e404a7d7.js";import{v as i}from"./variableTable.c39088cb.js";import{c as o,w as t,o as d,a as e,b as s,d as l,r}from"./index.e145a79b.js";const p=e("div",{class:"card"},[e("h5",{class:"text-right pr-30 mb-10"},"On this page:"),e("ul",null,[e("li",null,[e("a",{class:"link dark",href:"#default"}," Default")]),e("li",null,[e("a",{class:"link dark",href:"#label"}," Label ")]),e("li",null,[e("a",{class:"link dark",href:"#colors"}," Colors ")]),e("li",null,[e("a",{class:"link dark",href:"#flat"}," Flat ")]),e("li",null,[e("a",{class:"link dark",href:"#bordered"}," Bordered ")]),e("li",null,[e("a",{class:"link dark",href:"#square"}," Square ")]),e("li",null,[e("a",{class:"link dark",href:"#icon"}," Icon ")]),e("li",null,[e("a",{class:"link dark",href:"#variables"}," Variables ")])])],-1),u=e("section",{class:"section docs-title"},[e("div",{class:"container"},[e("h1",{class:"title-5 fw-500"},"Input"),e("p",{class:"subtitle"},[e("b",null,"GNB UI"),l(" has exciting input components.")])])],-1),m={class:"section"},b={class:"container"},h=e("h5",{class:"mb-20"},"To Import Inputs Separately:",-1),f={class:"section",id:"default"},v={class:"container"},_=e("div",null,[e("h2",{class:"mb-20"},"\u2022 Default Input"),e("p",{class:"fs-6"}," The default input component is a simple input field. ")],-1),x=e("div",{class:"mt-25"},[e("div",{class:"bg-white p-20 flex justify-center border-radius-2"},[e("div",{class:"form-element"},[e("input",{type:"text",class:"input",placeholder:"Name"})])])],-1),y={class:"mt-25"},g=e("p",null,"HTML:",-1),I={class:"section",id:"label"},N={class:"container"},w=e("div",null,[e("h2",{class:"mb-20"},"\u2022 Label"),e("p",{class:"fs-6"},[l(" To add a label to an input, add the "),e("code",{class:"text-danger"},"label"),l(" element with a "),e("code",{class:"text-danger"},".label"),l(" class inside the "),e("code",{class:"text-danger"},".form-element"),l(". ")])],-1),k=e("div",{class:"mt-25"},[e("div",{class:"bg-white p-20 flex justify-center border-radius-2"},[e("div",{class:"form-element"},[e("label",{for:"input_1",class:"label"},"Default Input"),e("input",{type:"text",class:"input",id:"input_1",placeholder:"Name"})])])],-1),T={class:"mt-25"},S=e("p",null,"HTML:",-1),D={class:"section",id:"colors"},$={class:"container"},q=e("div",null,[e("h2",{class:"mb-20"},"\u2022 Colors"),e("p",{class:"fs-6"},[l(" You can add a color to the input by adding the one of the color classes to the "),e("code",{class:"text-danger"},"input"),l(" element. Just click the input and see the change! ")])],-1),L=e("div",{class:"mt-25"},[e("div",{class:"bg-white p-20 flex wrap border-radius-2"},[e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Primary Input:"),e("input",{type:"text",class:"input primary",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Secondary Input:"),e("input",{type:"text",class:"input secondary",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Success Input:"),e("input",{type:"text",class:"input success",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Info Input:"),e("input",{type:"text",class:"input info",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Warning Input:"),e("input",{type:"text",class:"input warning",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Danger Input:"),e("input",{type:"text",class:"input danger",placeholder:"Name"})]),e("div",{class:"form-element"},[e("label",{class:"label"},"Dark Input:"),e("input",{type:"text",class:"input dark",placeholder:"Name"})])])],-1),B={class:"mt-25"},H=e("p",null,"HTML:",-1),M={class:"section",id:"flat"},j={class:"container"},P=e("div",null,[e("h2",{class:"mb-20"},"\u2022 Flat"),e("p",{class:"fs-6"},[l(" To use flat colors for input add "),e("code",{class:"text-danger"},".flat"),l(" with "),e("code",{class:"text-danger"},".input"),l(" class. ")])],-1),W=e("div",{class:"mt-25"},[e("div",{class:"bg-white p-20 flex wrap border-radius-2"},[e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Primary Input:"),e("input",{type:"text",class:"input primary flat",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Secondary Input:"),e("input",{type:"text",class:"input secondary flat",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Success Input:"),e("input",{type:"text",class:"input success flat",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Info Input:"),e("input",{type:"text",class:"input info flat",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Warning Input:"),e("input",{type:"text",class:"input warning flat",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Danger Input:"),e("input",{type:"text",class:"input danger flat",placeholder:"Name"})]),e("div",{class:"form-element"},[e("label",{class:"label"},"Dark Input:"),e("input",{type:"text",class:"input dark flat",placeholder:"Name"})])])],-1),V={class:"mt-25"},C=e("p",null,"HTML:",-1),A={class:"section",id:"bordered"},F={class:"container"},Y=e("div",null,[e("h2",{class:"mb-20"},"\u2022 Bordered"),e("p",{class:"fs-6"},[l(" To show bottom border always add "),e("code",{class:"text-danger"},".bordered"),l(" with "),e("code",{class:"text-danger"},".input"),l(" class and color class. You can use this feature for input state or for input with error. ")])],-1),z=e("div",{class:"mt-25"},[e("div",{class:"bg-white p-20 flex wrap border-radius-2"},[e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Primary Input:"),e("input",{type:"text",class:"input primary bordered",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Secondary Input:"),e("input",{type:"text",class:"input secondary bordered",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Success Input:"),e("input",{type:"text",class:"input success bordered",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Info Input:"),e("input",{type:"text",class:"input info bordered",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Warning Input:"),e("input",{type:"text",class:"input warning bordered",placeholder:"Name"})]),e("div",{class:"form-element mr-10"},[e("label",{class:"label"},"Danger Input:"),e("input",{type:"text",class:"input danger bordered",placeholder:"Name"})]),e("div",{class:"form-element"},[e("label",{class:"label"},"Dark Input:"),e("input",{type:"text",class:"input dark bordered",placeholder:"Name"})])])],-1),G={class:"mt-25"},J=e("p",null,"HTML:",-1),O={class:"section",id:"square"},U={class:"container"},E=e("div",null,[e("h2",{class:"mb-20"},"\u2022 Square"),e("p",{class:"fs-6"},[l(" To show your input as a square shape add "),e("code",{class:"text-danger"},".square"),l(" to the "),e("code",{class:"text-danger"},".input"),l(" element. ")])],-1),K=e("div",{class:"mt-25"},[e("div",{class:"bg-white p-20 flex justify-center border-radius-2"},[e("div",{class:"form-element"},[e("label",{for:"input_1",class:"label"},"Square Input"),e("input",{type:"text",class:"input primary square bordered",id:"input_1",placeholder:"Name"})])])],-1),Q={class:"mt-25"},R=e("p",null,"HTML:",-1),X={class:"section",id:"icon"},Z={class:"container"},ee=e("div",null,[e("h2",{class:"mb-20"},"\u2022 Icon"),e("p",{class:"fs-6"},[l(" To add icon to your input add "),e("code",{class:"text-danger"},".has-icon-before"),l(" or "),e("code",{class:"text-danger"},".has-icon-after"),l(" class to the "),e("code",{class:"text-danger"},".form-element"),l(" item. Then add icon inside the "),e("code",{class:"text-danger"},".input-icon"),l(" tags. ")])],-1),le=e("div",{class:"mt-25"},[e("div",{class:"bg-white p-20 flex justify-center border-radius-2"},[e("div",{class:"form-element has-icon-before mr-10"},[e("label",{class:"label"},"Icon Before:"),e("input",{type:"text",class:"input primary",placeholder:"Name"}),e("div",{class:"input-icon"},[e("i",{class:"ri-user-follow-line"})])]),e("div",{class:"form-element has-icon-after"},[e("label",{class:"label"},"Icon After:"),e("input",{type:"text",class:"input success",placeholder:"Name"}),e("div",{class:"input-icon"},[e("i",{class:"ri-user-follow-line"})])])])],-1),se={class:"mt-25"},ae=e("p",null,"HTML:",-1),te={class:"section",id:"variables"},ne={class:"container"},ce=e("div",null,[e("h2",{class:"mb-20"},"\u2022 Variables"),e("p",{class:"fs-6"}," To customize the input component you can change the variables listed below. ")],-1),me={__name:"Input",setup(ie){const n=[{name:"$input-background-color",value:"$gray-2",computed:"rgb(244, 247, 248)"},{name:"$input-height  <span class='tag success'>since 0.2.1</span>",value:"36px",computed:""},{name:"$input-placeholder-opacity",value:"0.4",computed:""},{name:"$input-min-width",value:"200px",computed:""},{name:"$input-border-radius",value:"12px",computed:""},{name:"$input-icon-box-width <span class='tag success'>since 0.2.1</span>",value:"$input-height",computed:"36px"},{name:"$input-icon-box-height <span class='tag success'>since 0.2.1</span>",value:"$input-icon-box-width",computed:"36px"},{name:"$input-border-width <span class='tag success'>since 0.2.1</span>",value:"2px",computed:""},{name:"$input-padding <span class='tag success'>since 0.2.1</span>",value:"7px 13px 7px 10px",computed:""}];return(oe,de)=>{const a=r("highlightjs");return d(),o(c,null,{navigation:t(()=>[p]),default:t(()=>[u,e("section",m,[e("div",b,[h,s(a,{autodetect:"",code:'@import "../../node_modules/gnb-ui/src/form/input";'})])]),e("section",f,[e("div",v,[_,x,e("div",y,[g,s(a,{autodetect:"",code:`<div class="form-element">
+    <input type="text" class="input" placeholder="Name" />
+</div>`})])])]),e("section",I,[e("div",N,[w,k,e("div",T,[S,s(a,{autodetect:"",code:`<div class="form-element">
+    <input type="text" class="input" placeholder="Name" />
+</div>`})])])]),e("section",D,[e("div",$,[q,L,e("div",B,[H,s(a,{autodetect:"",code:`<div class="form-element">
+    <label class="label">Primary Input:</label>
+    <input type="text" class="input primary" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Secondary Input:</label>
+    <input type="text" class="input secondary" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Success Input:</label>
+    <input type="text" class="input success" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Info Input:</label>
+    <input type="text" class="input info" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Warning Input:</label>
+    <input type="text" class="input warning" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Danger Input:</label>
+    <input type="text" class="input danger" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Dark Input:</label>
+    <input type="text" class="input dark" placeholder="Name" />
+</div>`})])])]),e("section",M,[e("div",j,[P,W,e("div",V,[C,s(a,{autodetect:"",code:`<div class="form-element">
+    <label class="label">Primary Input:</label>
+    <input type="text" class="input primary flat" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Secondary Input:</label>
+    <input type="text" class="input secondary flat" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Success Input:</label>
+    <input type="text" class="input success flat" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Info Input:</label>
+    <input type="text" class="input info flat" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Warning Input:</label>
+    <input type="text" class="input warning flat" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Danger Input:</label>
+    <input type="text" class="input danger flat" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Dark Input:</label>
+    <input type="text" class="input dark flat" placeholder="Name" />
+</div>`})])])]),e("section",A,[e("div",F,[Y,z,e("div",G,[J,s(a,{autodetect:"",code:`<div class="form-element">
+    <label class="label">Primary Input:</label>
+    <input type="text" class="input primary bordered" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Secondary Input:</label>
+    <input type="text" class="input secondary bordered" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Success Input:</label>
+    <input type="text" class="input success bordered" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Info Input:</label>
+    <input type="text" class="input info bordered" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Warning Input:</label>
+    <input type="text" class="input warning bordered" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Danger Input:</label>
+    <input type="text" class="input danger bordered" placeholder="Name" />
+</div>
+
+<div class="form-element">
+    <label class="label">Dark Input:</label>
+    <input type="text" class="input dark bordered" placeholder="Name" />
+</div>`})])])]),e("section",O,[e("div",U,[E,K,e("div",Q,[R,s(a,{autodetect:"",code:`<div class="form-element">
+    <label class="label">Square Input</label>
+    <input type="text" class="input primary square bordered" placeholder="Name" />
+</div>`})])])]),e("section",X,[e("div",Z,[ee,le,e("div",se,[ae,s(a,{autodetect:"",code:`<div class="form-element has-icon-before mr-10">
+    <label class="label">Icon Before:</label>
+    <input type="text" class="input primary" placeholder="Name" />
+    <div class="input-icon">
+        <i class="ri-user-follow-line"></i>
+    </div>
+</div>
+<div class="form-element has-icon-after">
+    <label class="label">Icon After:</label>
+    <input type="text" class="input success" placeholder="Name" />
+    <div class="input-icon">
+        <i class="ri-user-follow-line"></i>
+    </div>
+</div>`})])])]),e("section",te,[e("div",ne,[ce,s(i,{variables:n})])])]),_:1})}}};export{me as default};
---
+++ assets/Switch.d434642f.js
@@ -0,0 +1,130 @@
+import{_ as n}from"./Layout.e404a7d7.js";import{v as l}from"./variableTable.c39088cb.js";import{c as r,w as t,o,a as s,b as c,d as e,r as d}from"./index.e145a79b.js";const h=s("div",{class:"card"},[s("h5",{class:"text-right pr-30 mb-10"},"On this page:"),s("ul",null,[s("li",null,[s("a",{class:"link dark",href:"#default"}," Default")]),s("li",null,[s("a",{class:"link dark",href:"#colors"}," Colors ")]),s("li",null,[s("a",{class:"link dark",href:"#square"}," Square ")]),s("li",null,[s("a",{class:"link dark",href:"#sizing"}," Sizing ")]),s("li",null,[s("a",{class:"link dark",href:"#variables"}," Variables ")])])],-1),m=s("section",{class:"section docs-title"},[s("div",{class:"container"},[s("h1",{class:"title-5 fw-500"},"Switch"),s("p",{class:"subtitle"},[s("b",null,"GNB UI"),e(" has switch elements based on checkboxes. ")])])],-1),p={class:"section"},u={class:"container"},w=s("h5",{class:"mb-20"},"To Import Radio Separately:",-1),b={class:"section",id:"default"},f={class:"container"},v=s("div",null,[s("h2",{class:"mb-20"},"\u2022 Default Switch"),s("p",{class:"fs-6"}," The default switch component actually is a simple checkbox. But we changed the look and feel of the checkbox to make it look like a switch. "),s("p",{class:"fs-6"},[s("span",{class:"text-danger"},"*"),e("Do not forget to add "),s("code",{class:"text-danger"},"id"),e(" attribute to the "),s("code",{class:"text-danger"},"input"),e(" element and "),s("code",{class:"text-danger"},"for"),e(" attribute to the "),s("code",{class:"text-danger"},"label"),e(" element. ")])],-1),g=s("div",{class:"mt-25"},[s("div",{class:"bg-white p-20 flex justify-center border-radius-2"},[s("div",{class:"form-element"},[s("input",{type:"checkbox",id:"switch",class:"switch"}),s("label",{for:"switch"})])])],-1),y={class:"mt-25"},x=s("p",null,"HTML:",-1),_={class:"section",id:"colors"},k={class:"container"},q=s("div",null,[s("h2",{class:"mb-20"},"\u2022 Colors"),s("p",{class:"fs-6"},[e(" You can add a color to the switch by adding the one of the color classes to the "),s("code",{class:"text-danger"},".switch"),e(" element. Just click the switch and see the change! ")]),s("p",{class:"fs-6"},[s("span",{class:"text-danger"},"*"),e("Do not forget to add "),s("code",{class:"text-danger"},"id"),e(" attribute to the "),s("code",{class:"text-danger"},"input"),e(" element and "),s("code",{class:"text-danger"},"for"),e(" attribute to the "),s("code",{class:"text-danger"},"label"),e(" element. ")])],-1),$=s("div",{class:"mt-25"},[s("div",{class:"bg-white p-20 flex justify-center border-radius-2"},[s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-primary",class:"switch primary"}),s("label",{for:"switch-primary"})]),s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-secondary",class:"switch secondary"}),s("label",{for:"switch-secondary"})]),s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-success",class:"switch success"}),s("label",{for:"switch-success"})]),s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-info",class:"switch info"}),s("label",{for:"switch-info"})]),s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-warning",class:"switch warning"}),s("label",{for:"switch-warning"})]),s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-danger",class:"switch danger"}),s("label",{for:"switch-danger"})]),s("div",{class:"form-element"},[s("input",{type:"checkbox",id:"switch-dark",class:"switch dark"}),s("label",{for:"switch-dark"})])])],-1),T={class:"mt-25"},S=s("p",null,"HTML:",-1),D={class:"section",id:"square"},z={class:"container"},L=s("div",null,[s("h2",{class:"mb-20"},"\u2022 Square"),s("p",{class:"fs-6"},[e(" To show your switch as a square shape add "),s("code",{class:"text-danger"},".square"),e(" to the "),s("code",{class:"text-danger"},".switch"),e(" element. ")]),s("p",{class:"fs-6"},[s("span",{class:"text-danger"},"*"),e("Do not forget to add "),s("code",{class:"text-danger"},"id"),e(" attribute to the "),s("code",{class:"text-danger"},"input"),e(" element and "),s("code",{class:"text-danger"},"for"),e(" attribute to the "),s("code",{class:"text-danger"},"label"),e(" element. ")])],-1),j=s("div",{class:"mt-25"},[s("div",{class:"bg-white p-20 flex justify-center border-radius-2"},[s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-primary-square",class:"switch primary square"}),s("label",{for:"switch-primary-square"})]),s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-secondary-square",class:"switch secondary square"}),s("label",{for:"switch-secondary-square"})]),s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-success-square",class:"switch success square"}),s("label",{for:"switch-success-square"})]),s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-info-square",class:"switch info square"}),s("label",{for:"switch-info-square"})]),s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-warning-square",class:"switch warning square"}),s("label",{for:"switch-warning-square"})]),s("div",{class:"form-element mr-10"},[s("input",{type:"checkbox",id:"switch-danger-square",class:"switch danger square"}),s("label",{for:"switch-danger-square"})]),s("div",{class:"form-element"},[s("input",{type:"checkbox",id:"switch-dark-square",class:"switch dark square"}),s("label",{for:"switch-dark-square"})])])],-1),M={class:"mt-25"},B=s("p",null,"HTML:",-1),C={class:"section",id:"sizing"},V={class:"container"},H=s("div",null,[s("h2",{class:"mb-20"},"\u2022 Sizing"),s("p",{class:"fs-6"},[e(" We have 5 size of switches. Theese are: "),s("br"),s("code",{class:"text-danger"},".mini"),e(),s("br"),s("code",{class:"text-danger"},".sm"),e(),s("br"),s("code",{class:"text-danger"},".lg"),e(),s("br"),s("code",{class:"text-danger"},".xl"),e(),s("br"),e(" and default size. ")]),s("p",{class:"fs-6"},[s("span",{class:"text-danger"},"*"),e("Do not forget to add "),s("code",{class:"text-danger"},"id"),e(" attribute to the "),s("code",{class:"text-danger"},"input"),e(" element and "),s("code",{class:"text-danger"},"for"),e(" attribute to the "),s("code",{class:"text-danger"},"label"),e(" element. ")])],-1),N=s("div",{class:"mt-25"},[s("div",{class:"bg-white p-20 flex wrap justify-center border-radius-2"},[s("div",{class:"form-element mr-30"},[s("span",null,"Mini:"),s("input",{type:"checkbox",id:"switch-primary-mini",class:"switch primary mini"}),s("label",{for:"switch-primary-mini"})]),s("div",{class:"form-element mr-30"},[s("span",null,"Small:"),s("input",{type:"checkbox",id:"switch-primary-sm",class:"switch primary sm"}),s("label",{for:"switch-primary-sm"})]),s("div",{class:"form-element mr-30"},[s("span",null,"Default:"),s("input",{type:"checkbox",id:"switch-primary-default",class:"switch primary"}),s("label",{for:"switch-primary-default"})]),s("div",{class:"form-element mr-30"},[s("span",null,"Large:"),s("input",{type:"checkbox",id:"switch-primary-lg",class:"switch primary lg"}),s("label",{for:"switch-primary-lg"})]),s("div",{class:"form-element mr-30"},[s("span",null,"XLarge:"),s("input",{type:"checkbox",id:"switch-primary-xl",class:"switch primary xl"}),s("label",{for:"switch-primary-xl"})])])],-1),I={class:"mt-25"},X=s("p",null,"HTML:",-1),G={class:"section",id:"variables"},J={class:"container"},O=s("div",null,[s("h2",{class:"mb-20"},"\u2022 Variables"),s("p",{class:"fs-6"}," To customize the switch component you can change the variables listed below. ")],-1),F={__name:"Switch",setup(R){const i=[{name:"$switch-width  <span class='tag success'>since 0.2.1</span>",value:"50px",computed:""},{name:"$switch-border-radius  <span class='tag success'>since 0.2.1</span>",value:"200px",computed:""},{name:"$switch-indicator-width  <span class='tag success'>since 0.2.1</span>",value:"17px",computed:""},{name:"$switch-background-color  <span class='tag success'>since 0.2.1</span>",value:"$gray-1",computed:"rgb(230, 233, 234)"},{name:"$switch-checked-background-color  <span class='tag success'>since 0.2.1</span>",value:"$gray-3",computed:"rgb(108, 117, 125)"},{name:"$switch-indicator-color  <span class='tag success'>since 0.2.1</span>",value:"$white",computed:"rgb(255, 255, 255)"},{name:"$switch-indicator-border-radius  <span class='tag success'>since 0.2.1</span>",value:"27px",computed:""},{name:"$mini-switch-width  <span class='tag success'>since 0.2.1</span>",value:"30px",computed:""},{name:"$sm-switch-width  <span class='tag success'>since 0.2.1</span>",value:"40px",computed:""},{name:"$lg-switch-width  <span class='tag success'>since 0.2.1</span>",value:"70px",computed:""},{name:"$xl-switch-width  <span class='tag success'>since 0.2.1</span>",value:"80px",computed:""}];return(U,W)=>{const a=d("highlightjs");return o(),r(n,null,{navigation:t(()=>[h]),default:t(()=>[m,s("section",p,[s("div",u,[w,c(a,{autodetect:"",code:'@import "../../node_modules/gnb-ui/src/form/radio";'})])]),s("section",b,[s("div",f,[v,g,s("div",y,[x,c(a,{autodetect:"",code:`<div class="form-element horizontal">
+  <div class="checkbox-group">
+    <input type="checkbox" id="check_1" class="checkbox" />
+    <label class="label" for="check_1">Default Checkbox</label>
+  </div>
+</div>`})])])]),s("section",_,[s("div",k,[q,$,s("div",T,[S,c(a,{autodetect:"",code:`<div class="form-element">
+    <input
+      type="checkbox"
+      id="switch-primary"
+      class="switch primary"
+    />
+    <label for="switch-primary"></label>
+</div>
+
+<div class="form-element">
+    <input
+      type="checkbox"
+      id="switch-secondary"
+      class="switch secondary"
+    />
+    <label for="switch-secondary"></label>
+</div>
+
+<div class="form-element">
+    <input
+      type="checkbox"
+      id="switch-success"
+      class="switch success"
+    />
+    <label for="switch-success"></label>
+</div>
+
+<div class="form-element">
+    <input type="checkbox" id="switch-info" class="switch info" />
+    <label for="switch-info"></label>
+</div>
+
+<div class="form-element">
+    <input
+      type="checkbox"
+      id="switch-warning"
+      class="switch warning"
+    />
+    <label for="switch-warning"></label>
+</div>
+
+<div class="form-element">
+    <input type="checkbox" id="switch-danger" class="switch danger" />
+    <label for="switch-danger"></label>
+</div>
+
+<div class="form-element">
+    <input type="checkbox" id="switch-dark" class="switch dark" />
+    <label for="switch-dark"></label>
+  </div>
+</div>`})])])]),s("section",D,[s("div",z,[L,j,s("div",M,[B,c(a,{autodetect:"",code:`<div class="form-element">
+    <input
+      type="checkbox"
+      id="switch-primary-square"
+      class="switch primary square"
+    />
+    <label for="switch-primary-square"></label>
+</div>
+
+<div class="form-element">
+    <input
+      type="checkbox"
+      id="switch-secondary-square"
+      class="switch secondary square"
+    />
+    <label for="switch-secondary-square"></label>
+</div>
+
+<div class="form-element">
+    <input
+      type="checkbox"
+      id="switch-success-square"
+      class="switch success square"
+    />
+    <label for="switch-success-square"></label>
+</div>
+
+<div class="form-element">
+    <input type="checkbox" id="switch-info-square" class="switch info square" />
+    <label for="switch-info-square"></label>
+</div>
+
+<div class="form-element">
+    <input
+      type="checkbox"
+      id="switch-warning-square"
+      class="switch warning square"
+    />
+    <label for="switch-warning-square"></label>
+</div>
+
+<div class="form-element">
+    <input type="checkbox" id="switch-danger-square" class="switch danger square" />
+    <label for="switch-danger-square"></label>
+</div>
+
+<div class="form-element">
+    <input type="checkbox" id="switch-dark-square" class="switch dark square" />
+    <label for="switch-dark-square"></label>
+  </div>
+</div>`})])])]),s("section",C,[s("div",V,[H,N,s("div",I,[X,c(a,{autodetect:"",code:`<div class="form-element">
+  <span>Mini:</span>
+  <input type="checkbox" id="switch-primary-mini" class="switch primary mini">
+  <label for="switch-primary-mini"></label>
+</div>
+<div class="form-element">
+  <span>Small:</span>
+  <input type="checkbox" id="switch-primary-sm" class="switch primary sm">
+  <label for="switch-primary-sm"></label>
+</div>
+<div class="form-element">
+  <span>Default:</span>
+  <input type="checkbox" id="switch-primary-default" class="switch primary">
+  <label for="switch-primary-default"></label>
+</div>
+<div class="form-element">
+  <span>Large:</span>
+  <input type="checkbox" id="switch-primary-lg" class="switch primary lg">
+  <label for="switch-primary-lg"></label>
+</div>
+<div class="form-element">
+  <span>XLarge:</span>
+  <input type="checkbox" id="switch-primary-xl" class="switch primary xl">
+  <label for="switch-primary-xl"></label>
+</div>`})])])]),s("section",G,[s("div",J,[O,c(l,{variables:i})])])]),_:1})}}};export{F as default};
